c0c0c80e6e2fc792979f89e39074e563198f1d00
Nicolas Schejtman <nschejtman93@gmail.com>
2018-07-25 12:48:55 -0300

    First packaging

---
+++ src/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py-v8n",
+    version="0.0.1",
+    author="Nicolas A. Schejtman",
+    author_email="nschejtman93@gmail.com",
+    description="Python fluent validation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nschejtman/py-v8n",
+    packages=['py_v8n'],
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
